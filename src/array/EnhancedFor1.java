4b0ed238e757c1f933d1d07526a47f3f5fc74a10
skymoon7337 <skymoon7337@gmail.com>
2025-07-08 00:05:44 +0900

    edit

--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -3,7 +3,7 @@ package array;
 public class EnhancedFor1 {
     public static void main(String[] args) {
 
-        int[] numbers={1,2,3,4,5,};
+        int[] numbers={1,2,3,4,5};
 
         for(int i=0; i<numbers.length;i++){
             int number=numbers[i];
