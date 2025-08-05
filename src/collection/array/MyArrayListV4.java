a855e613037c077504fc9c4682d8c62f0da87d31
skymoon7337 <skymoon7337@gmail.com>
2025-08-06 01:14:40 +0900

    add

---
+++ src/collection/array/MyArrayListV4.java
@@ -0,0 +1,102 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV4<E>{
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV4(){
+        elementData = new Object[DEFAULT_CAPACITY]; //생성자에 E 불가능
+    }
+
+    public MyArrayListV4(int initialCapacity){
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void add(E e){
+        if (size == elementData.length){
+            grow();
+        }
+
+        elementData[size] = e;
+        size++;
+    }
+
+    //코드 추가
+    public void add(int index, Object e){
+        if (size == elementData.length){
+            grow();
+        }
+        // 데이터 이동
+        shiftRightFrom(index);
+
+        elementData[index] = e;
+        size++;
+    }
+
+    // 코드 추가, 요소의 마지막부터 index까지 오른쪽으로 밀기
+    private void shiftRightFrom(int index) {
+        if (size>=index){
+            for (int i = size; i > index; i--) {
+                elementData[i] = elementData[i-1];
+            }
+        }
+    }
+
+
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData= Arrays.copyOf(elementData, newCapacity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index){
+        return (E) elementData[index]; // 생성자에 E 불가능
+    }
+
+    public E set(int index, E element){
+        E oldValue = get(index);
+        elementData[index]=element;
+        return oldValue;
+    }
+
+    // 코드 추가
+    public E remove(int index){
+        E oldValue = get(index);
+        // 데이터 이동 index<size , 같으면 ㄱㅊ 이동은 오른쪽에서 왼쪽 맨 왼쪽에 먼저
+        shiftLeftFrom(index);
+        size--;
+        elementData[size]=null;
+        return oldValue;
+    }
+
+    // 코드 추가 // 요소의 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size-1; i++) {
+            elementData[i] = elementData[i+1];
+        }
+    }
+
+    public int indexOf(E o){
+        for (int i = 0; i < size; i++) {
+            if(o.equals(elementData[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String toString(){
+       return Arrays.toString(Arrays.copyOf(elementData,size))+
+               " size=" + size + ", capacity="+ elementData.length;
+    }
+}
